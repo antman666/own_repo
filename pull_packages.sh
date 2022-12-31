f21fa61aa4e7900baab2dfb028aca9cf7cb03b75
antman666 <945360554@qq.com>
2022-12-31 14:15:21 +0800

    update repo

---
+++ pull_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url="https://github.com/antman666/action-repo/releases/download/x86_64"
+wget -c ${url}/gcc-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-ada-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-fortran-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-go-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-libs-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/gcc-objc-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/lib32-gcc-libs-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/libgccjit-12.2.1-2-x86_64.pkg.tar.zst
+wget -c ${url}/libtool-2.4.7-1-x86_64.pkg.tar.zst
